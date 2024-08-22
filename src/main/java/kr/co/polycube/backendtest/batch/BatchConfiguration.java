1077ac3770dd8c6cdac0020b4fbd3506278a7e7f
kulta710 <kulta710@gmail.com>
2024-08-22 11:49:42 +0900

    batch

--- src/main/java/kr/co/polycube/backendtest/batch/BatchConfiguration.java
+++ src/main/java/kr/co/polycube/backendtest/batch/BatchConfiguration.java
@@ -1,24 +1,41 @@
 //package kr.co.polycube.backendtest.batch;
 //
 //import org.springframework.batch.core.Job;
-//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+//import org.springframework.batch.core.job.builder.JobBuilder;
+//import org.springframework.batch.core.repository.JobRepository;
+//import org.springframework.batch.core.step.builder.StepBuilder;
+//import org.springframework.batch.core.step.tasklet.Tasklet;
+//import org.springframework.batch.repeat.RepeatStatus;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
+//import org.springframework.transaction.PlatformTransactionManager;
+//
+//import java.util.concurrent.TimeUnit;
 //
 //@Configuration
+//@EnableBatchProcessing
 //public class BatchConfiguration {
 //
-//    private final JobBuilderFactory jobBuilderFactory;
-//    private final StepBuilderFactory stepBuilderFactory;
-//
 //    @Bean
-//    public Job simpleJob() {
-//        return jobBuilderFactory.get("simpleJob").start(simpleStep()).build();
+//    public Job simpleJob(JobRepository jobRepository, Step step) {
+//        return new JobBuilder("simpleJob", jobRepository).start(step).build();
 //    }
 //
 //    @Bean
-//    public Step simpleStep() {
-//        return stepBuilderFactory.get("simpleStep").tasklet(())
+//    public Step simpleStep(JobRepository jobRepository, Tasklet simpleTasklet, PlatformTransactionManager transactionManager) {
+//        return new StepBuilder("simpleStep", jobRepository).tasklet(simpleTasklet, transactionManager).build();
+//    }
+//
+//    @Bean
+//    public Tasklet simpleTasklet() {
+//        return (stepContribution, chunkContext) -> {
+//            for (int i = 10; i > 0; i--) {
+//                TimeUnit.SECONDS.sleep(1);
+//                System.out.println(i);
+//            }
+//            return RepeatStatus.FINISHED;
+//        };
 //    }
 //}
